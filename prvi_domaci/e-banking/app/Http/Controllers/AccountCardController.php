16825c6cf73251ee6b74d0dfeab577bbf6436873
Milos Damjanovic <md20210126@student.fon.bg.ac.rs>
2025-01-14 17:57:12 +0100

    zaboravljeni fajl

--- prvi_domaci/e-banking/app/Http/Controllers/AccountCardController.php
+++ prvi_domaci/e-banking/app/Http/Controllers/AccountCardController.php
@@ -9,25 +9,25 @@ class AccountCardController extends Controller
 {
     
     public function index(Request $request)
-{
-    $user = $request->user();
+    {
+        $user = $request->user();
 
-    $accounts = $user->accounts;
+        $accounts = $user->accounts;
 
-    if ($accounts->isEmpty()) {
-        return response()->json(['message' => 'No accounts found for the user'], 404);
+        if ($accounts->isEmpty()) {
+            return response()->json(['message' => 'No accounts found for the user'], 404);
+        }
+
+        $cards = $accounts->flatMap(function ($account) {
+            return $account->cards;
+        });
+
+        if ($cards->isEmpty()) {
+            return response()->json(['message' => 'No cards found for the account'], 404);
+        }
+
+        return response()->json(['message' => 'Cards retrieved successfully', 'data' => $cards], 200);
     }
-
-    $cards = $accounts->flatMap(function ($account) {
-        return $account->cards;
-    });
-
-    if ($cards->isEmpty()) {
-        return response()->json(['message' => 'No cards found for the account'], 404);
-    }
-
-    return response()->json(['message' => 'Cards retrieved successfully', 'data' => $cards], 200);
-}
 
 
 }
