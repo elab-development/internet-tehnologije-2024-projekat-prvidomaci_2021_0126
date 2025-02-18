e6983f3ebe35d75915636fd94e5fca1048cf6a9d
Milos Damjanovic <md20210126@student.fon.bg.ac.rs>
2025-02-18 21:30:14 +0100

    kreiranje forme za obavljanje nove transakcije

--- drugi_domaci/e-banking/src/pages/NewTransaction.js
+++ drugi_domaci/e-banking/src/pages/NewTransaction.js
@@ -1,7 +1,51 @@
-import React from 'react'
+import React,{useState} from 'react'
+
+function handleSubmit(){
+
+}
 
 function NewTransaction() {
 
+    const [recipient, setRecipient] = useState('');
+    // const [dateTime, setDateTime] = useState('');
+    const [amount, setAmount] = useState('');
+
+    return (
+        <div className="new-transaction-container">
+          <h2>Create New Transaction</h2>
+          <form onSubmit={handleSubmit} className="new-transaction-form">
+            
+            <div className="form-group">
+              <label>Recipient:</label>
+              <input
+                type="text"
+                id="recipient"
+                value={recipient}
+                onChange={(e) => setRecipient(e.target.value)}
+                placeholder="Enter recipient name"
+                required
+              />
+            </div>
+    
+            <div className="form-group">
+              <label>Amount:</label>
+              <input
+                type="number"
+                id="amount"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                placeholder="Enter amount"
+                min="0"
+                step="1"
+                required
+              />
+            </div>
+    
+    
+            <button type="submit" className="submit-button">Submit</button>
+          </form>
+        </div>
+      );
 }
 
 export default NewTransaction
