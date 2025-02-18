de7093afc6f64dcd7fb84538652830863fa20426
Milos Damjanovic <md20210126@student.fon.bg.ac.rs>
2025-02-18 20:19:18 +0100

    Inicijalizacija osnovne strukture frontend aplikacije

--- drugi_domaci/e-banking/src/App.js
+++ drugi_domaci/e-banking/src/App.js
@@ -1,7 +1,42 @@
 import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import Cards from './pages/Cards';
+import NewTransaction from './pages/NewTransaction';
+import Transactions from './pages/Transactions';
+import Profile from './pages/Profile';
+
 
 function App() {
+
+
+  return (
+    <div className="App">    
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<>
+            <Home/>
+          </>} />
+          <Route path="/cards" element={<>
+            {<Cards/>}
+          </>} />
+          <Route path="/transactions" element={<>
+            {<Transactions/> }
+          </>} />
+          <Route path="/new-transaction" element={<>
+            {<NewTransaction/>}
+          </>} />
+          <Route path="/profile" element={<>
+            <Profile/>
+            
+          </>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 
 }
 
---
+++ drugi_domaci/e-banking/src/pages/Cards.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+function Cards() {
+}
+
+export default Cards
